48c3fe10b2aafa39041b4853ab67997d6c9b7bde
Michael W Powell <mwpowellhtx@gmail.com>
2019-05-10 19:06:22 -0400

    added a slightly more exotic key value pair option registration

---
+++ src/NConsole.Options.Tests/StringComparisonCultureTypesKeyValueOptionRegistrationTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace NConsole.Options
+{
+    using Xunit.Abstractions;
+
+    public class StringComparisonCultureTypesKeyValueOptionRegistrationTests : KeyValueOptionRegistrationTestFixtureBase<StringComparison, CultureTypes>
+    {
+        public StringComparisonCultureTypesKeyValueOptionRegistrationTests(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+    }
+}
