8759d1707875db4b32372c96987d2ae457874b68
Michael W Powell <mwpowellhtx@gmail.com>
2019-05-16 17:09:44 -0400

    extended simple argument parsing from simple registration of the same it is nearly a hybrid with the target oriented option but not quite or at least not enough to justify confusing option callbacks in the tests added simple parsing test cases and tests

--- src/NConsole.Options.Tests/Parsing/Simple/SimpleArgumentParsingTestFixtureBase.cs
+++ src/NConsole.Options.Tests/Parsing/Simple/SimpleArgumentParsingTestFixtureBase.cs
@@ -1,14 +1,59 @@
-﻿namespace NConsole.Options.Parsing.Simple
+﻿using System.Collections.Generic;
+
+namespace NConsole.Options.Parsing.Simple
 {
+    using Registration.Simple;
     using Xunit;
     using Xunit.Abstractions;
-    using NConsole.Options.Registration.Simple;
 
     public abstract class SimpleArgumentParsingTestFixtureBase : SimpleOptionRegistrationTestFixtureBase
     {
+        // ReSharper disable once RedundantEmptyObjectOrCollectionInitializer
+        protected ICollection<bool> OptionsVisited { get; } = new List<bool> { };
+
+        private void VerifyOptionsVisited(IEnumerable<bool> expected, IEnumerable<bool> actual)
+            => actual.AssertNotNull().AssertEqual(expected.AssertNotNull());
+
         protected SimpleArgumentParsingTestFixtureBase(ITestOutputHelper outputHelper)
             : base(outputHelper)
         {
         }
+
+#pragma warning disable xUnit1003
+        /// <summary>
+        /// Verifies that the <see cref="OptionSet"/> Can Parse the <paramref name="args"/>.
+        /// </summary>
+        /// <param name="prototype"></param>
+        /// <param name="description"></param>
+        /// <param name="args"></param>
+        /// <param name="expectedValues">Simply reflects whether we encountered
+        /// the expected <see cref="Option"/> visitation during parsing.</param>
+        /// <param name="expectedUnprocessedArgs"></param>
+        [Theory]
+        public virtual void Can_Parse_Arguments(string prototype, string description
+            , string[] args, bool[] expectedValues, string[] expectedUnprocessedArgs
+        )
+        {
+            OptionsVisited.AssertNotNull();
+
+            // The Option Registration verifies Prototype, Description, etc.
+            args.AssertNotNull().AssertNotEmpty();
+            expectedValues.AssertNotNull();
+            expectedUnprocessedArgs.AssertNotNull();
+
+            // Clears the Parsed Values prior to the Next Parsing Attempt.
+            void VerifyParsingResults(OptionSet options)
+            {
+                OptionsVisited.Clear();
+                var actualUnprocessedArgs = options.Parse(args).AssertNotNull();
+                Assert.Equal(expectedValues, OptionsVisited);
+                Assert.Equal(expectedUnprocessedArgs, actualUnprocessedArgs);
+            }
+
+            VerifyParsingResults(Register(prototype));
+            VerifyParsingResults(Register(prototype, description));
+        }
+#pragma warning restore xUnit1003
+
     }
 }
---
+++ src/NConsole.Options.Tests/Parsing/Simple/SimpleArgumentParsingTests.cs
@@ -0,0 +1,27 @@
+﻿namespace NConsole.Options.Parsing.Simple
+{
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class SimpleArgumentParsingTests : SimpleArgumentParsingTestFixtureBase
+    {
+        public SimpleArgumentParsingTests(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+#pragma warning disable xUnit1008
+        /// <inheritdoc />
+        [ClassData(typeof(Data.Parsing.SimpleOptionSetParsingTestCases))]
+        public override void Can_Parse_Arguments(string prototype, string description
+            , string[] args, bool[] expectedValues, string[] expectedUnprocessedArgs
+        )
+        {
+            Callback = () => OptionsVisited.Add(true);
+
+            base.Can_Parse_Arguments(prototype, description, args, expectedValues, expectedUnprocessedArgs);
+        }
+#pragma warning restore xUnit1008
+
+    }
+}
