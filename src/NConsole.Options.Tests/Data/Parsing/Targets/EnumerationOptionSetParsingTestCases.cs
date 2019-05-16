132ccd011b946a22d7c270cfb36975730ed1d3a5
Michael W Powell <mwpowellhtx@gmail.com>
2019-05-16 15:19:07 -0400

    added enumeration based targets argument parsing tests

---
+++ src/NConsole.Options.Tests/Data/Parsing/Targets/EnumerationOptionSetParsingTestCases.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NConsole.Options.Data.Parsing.Targets
+{
+    /// <summary>
+    /// We could use any Ordinal based Enumeration for this purpose, but we will use
+    /// <see cref="StringComparison"/> for test purposes, as it is pretty much stock,
+    /// and has a sufficiently nominal range of values to make testing worthwhile.
+    /// </summary>
+    internal class EnumerationOptionSetParsingTestCases
+        : RequiredOrOptionalOptionSetParsingTestCasesBase<StringComparison>
+    {
+        private static string RenderValue(StringComparison value) => value.ToString();
+
+        // ReSharper disable once IdentifierTypo
+        protected override string RenderOneWordUnbunbledArgument(string prefix, string prototype
+            , char requiredOrOptional, StringComparison value)
+            => $"{prefix}{prototype}{requiredOrOptional}{RenderValue(value)}";
+
+        protected override string RenderTwoWordValuePhrase(StringComparison value) => RenderValue(value);
+
+        protected override IEnumerable<StringComparison> GetNominalValueRange()
+        {
+            foreach(StringComparison value in Enum.GetValues(typeof(StringComparison)))
+            {
+                yield return value;
+            }
+        }
+    }
+}
