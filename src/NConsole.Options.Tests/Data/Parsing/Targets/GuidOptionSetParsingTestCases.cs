8650317c1f607d4a8d6fc3ae2ef560e97bc60340
Michael W Powell <mwpowellhtx@gmail.com>
2019-05-20 12:51:57 -0400

    added UUID based parsing tests

---
+++ src/NConsole.Options.Tests/Data/Parsing/Targets/GuidOptionSetParsingTestCases.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NConsole.Options.Data.Parsing.Targets
+{
+    internal class GuidOptionSetParsingTestCases : RequiredOrOptionalOptionSetParsingTestCasesBase<Guid>
+    {
+        protected override string RenderValue(Guid value) => $"{value:N}";
+
+        protected override IEnumerable<Guid> GetNominalValueRange()
+        {
+            yield return Guid.NewGuid();
+            yield return Guid.NewGuid();
+        }
+    }
+}
