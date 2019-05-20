2668029aa78fd4a8d4808cf886bd3796e5c51390
Michael W Powell <mwpowellhtx@gmail.com>
2019-05-20 18:27:23 -0400

    stubbed in key value pair modules either commented out or literally stubbed in

---
+++ src/NConsole.Options.Tests/Data/Parsing/RequiredOrOptionalOptionSetParsingTestCasesBase.KeyValue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace NConsole.Options.Data.Parsing
+{
+    internal abstract class RequiredOrOptionalOptionSetParsingTestCasesBase<TKey, TValue>
+        : RequiredOrOptionalOptionSetParsingTestCasesBase<KeyValuePair<TKey, TValue>>
+    {
+        protected abstract string RenderKey(TKey key);
+
+        /// <summary>
+        /// Not to be confused with
+        /// <see cref="RequiredOrOptionalOptionSetParsingTestCasesBase{T}.RenderValue"/>.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        protected abstract string RenderValue(TValue value);
+
+        protected sealed override IEnumerable<string> RenderValue(KeyValuePair<TKey, TValue> pair)
+        {
+            var (key, value) = pair;
+            yield return RenderKey(key);
+            yield return RenderValue(value);
+        }
+    }
+}
