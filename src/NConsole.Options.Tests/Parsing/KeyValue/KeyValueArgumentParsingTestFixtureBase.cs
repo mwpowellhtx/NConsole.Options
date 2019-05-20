d1f61116feb33d0bf890cb1b55d4c4a5eda9680f
Michael W Powell <mwpowellhtx@gmail.com>
2019-05-20 12:51:37 -0400

    added key value pair parsing framework

--- src/NConsole.Options.Tests/Parsing/KeyValue/KeyValueArgumentParsingTestFixtureBase.cs
+++ src/NConsole.Options.Tests/Parsing/KeyValue/KeyValueArgumentParsingTestFixtureBase.cs
@@ -1,14 +1,75 @@
-﻿namespace NConsole.Options.Parsing.KeyValue
+﻿using System.Collections.Generic;
+
+namespace NConsole.Options.Parsing.KeyValue
 {
+    using Registration.KeyValue;
     using Xunit;
     using Xunit.Abstractions;
-    using NConsole.Options.Registration.KeyValue;
 
-    public abstract class KeyValueArgumentParsingTestFixtureBase<TKey, TValue> : KeyValueOptionRegistrationTestFixtureBase<TKey, TValue>
+    public abstract class KeyValueArgumentParsingTestFixtureBase<TKey, TValue, TEqualityComparer>
+        : KeyValueOptionRegistrationTestFixtureBase<TKey, TValue>
+        where TEqualityComparer : class, IEqualityComparer<IEnumerable<KeyValuePair<TKey, TValue>>>, new()
     {
+        // ReSharper disable once RedundantEmptyObjectOrCollectionInitializer
+        protected virtual ICollection<KeyValuePair<TKey, TValue>> PairsVisited { get; }
+            = new List<KeyValuePair<TKey, TValue>> { };
+
+        protected abstract class PairComparerBase : IEqualityComparer<IEnumerable<KeyValuePair<TKey, TValue>>>
+        {
+            public abstract bool Equals(IEnumerable<KeyValuePair<TKey, TValue>> x
+                , IEnumerable<KeyValuePair<TKey, TValue>> y);
+
+            public abstract int GetHashCode(IEnumerable<KeyValuePair<TKey, TValue>> obj);
+        }
+
+        protected virtual void VerifyPairsVisited(IEnumerable<KeyValuePair<TKey, TValue>> expected
+            , IEnumerable<KeyValuePair<TKey, TValue>> actual)
+        {
+            // ReSharper disable once RedundantEmptyObjectOrCollectionInitializer
+            actual.AssertEqual(expected, new TEqualityComparer { });
+        }
+
         protected KeyValueArgumentParsingTestFixtureBase(ITestOutputHelper outputHelper)
             : base(outputHelper)
         {
         }
+
+#pragma warning disable xUnit1003
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="prototype"></param>
+        /// <param name="description"></param>
+        /// <param name="args"></param>
+        /// <param name="requiredOrOptional"></param>
+        /// <param name="separators"></param>
+        /// <param name="expectedUnprocessedArgs"></param>
+        [Theory]
+        public virtual void Can_Parse_Arguments(string prototype, string description, string[] args
+            , char? requiredOrOptional, string separators, KeyValuePair<TKey, TValue>[] expectedPairs
+            , string[] expectedUnprocessedArgs
+        )
+        {
+            PairsVisited.AssertNotNull().AssertEmpty();
+
+            args.AssertNotNull().AssertNotEmpty();
+            expectedPairs.AssertNotNull();
+            expectedUnprocessedArgs.AssertNotNull();
+
+            // Clears the Parsed Values prior to the Next Parsing Attempt.
+            void VerifyParsingResults(OptionSet options)
+            {
+                PairsVisited.Clear();
+                var actualUnprocessedArgs = options.Parse(args).AssertNotNull();
+                // ReSharper disable once RedundantEmptyObjectOrCollectionInitializer
+                PairsVisited.AssertEqual(expectedPairs, new TEqualityComparer { });
+                actualUnprocessedArgs.AssertNotNull().ToArray().AssertEqual(expectedUnprocessedArgs);
+            }
+
+            VerifyParsingResults(Register(prototype, requiredOrOptional, separators));
+            VerifyParsingResults(Register(prototype, description, requiredOrOptional, separators));
+        }
+#pragma warning restore xUnit1003
+
     }
 }
