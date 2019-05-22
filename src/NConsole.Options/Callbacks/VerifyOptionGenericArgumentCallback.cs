4af1413bd8b74bf1545a1e742925fa554764ffe4
Michael W Powell <mwpowellhtx@gmail.com>
2019-05-22 17:30:31 -0400

    added generic argument verification callback added reflection extension method for purposes of verifying generic argument types

---
+++ src/NConsole.Options/Callbacks/VerifyOptionGenericArgumentCallback.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace NConsole.Options
+{
+    /// <summary>
+    /// Verify the <paramref name="genericType"/> is what we think it is.
+    /// </summary>
+    /// <param name="genericType"></param>
+    /// <returns></returns>
+    internal delegate bool VerifyOptionGenericArgumentCallback(Type genericType);
+}
