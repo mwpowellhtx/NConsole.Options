bd92c25919fb9c1b6fff8bf9f102c49b52328a46
Michael W Powell <mwpowellhtx@gmail.com>
2019-05-22 18:52:17 -0400

    added try parse extension methods

---
+++ src/NConsole.Options/Extensions/OptionSetExtensionMethods.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace NConsole.Options
+{
+    public static class OptionSetExtensionMethods
+    {
+        /// <summary>
+        /// Tries to <see cref="OptionSet.Parse"/> the <paramref name="args"/>, receiving the
+        /// <paramref name="unprocessedArgs"/>.
+        /// </summary>
+        /// <param name="options"></param>
+        /// <param name="args"></param>
+        /// <param name="unprocessedArgs"></param>
+        /// <returns></returns>
+        public static bool TryParse(this OptionSet options, IEnumerable<string> args, out IEnumerable<string> unprocessedArgs)
+        {
+            // ReSharper disable PossibleMultipleEnumeration
+            unprocessedArgs = options.Parse(args.ToArray());
+            return unprocessedArgs.Count() != args.Count();
+            // ReSharper restore PossibleMultipleEnumeration
+        }
+
+        /// <summary>
+        /// Tries to <see cref="OptionSet.Parse"/> the <paramref name="args"/>. Any unprocessed
+        /// <paramref name="args"/> are effectively ignored and discarded.
+        /// </summary>
+        /// <param name="options"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static bool TryParse(this OptionSet options, params string[] args) => options.TryParse(args, out _);
+    }
+}
