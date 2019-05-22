91f96102dcf40da6dca6f4a522717c5b545278c6
Michael W Powell <mwpowellhtx@gmail.com>
2019-05-22 12:17:20 -0400

    refactored based on askew naming convention added a couple more test cases

---
+++ src/NConsole.Options.Tests/Data/Parsing/KeyValue/UniversalUniqueIdentifierIntegerOptionSetParsingTestCases.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NConsole.Options.Data.Parsing.KeyValue
+{
+    internal class UniversalUniqueIdentifierIntegerOptionSetParsingTestCases : RequiredOrOptionalOptionSetParsingTestCasesBase<Guid, int>
+    {
+        protected override IEnumerable<Guid> NominalPairKeys => ProtectedUniversalUniqueIdentifiers;
+
+        protected override IEnumerable<int> NominalPairValues => ProtectedIntegers;
+
+        protected override string RenderKey(Guid key) => RenderUniversalUniqueIdentifier(key);
+
+        protected override string RenderValue(int value) => RenderInteger(value);
+    }
+}
