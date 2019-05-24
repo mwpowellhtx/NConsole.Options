1760fca10ba21f790613f8a9da4f0997b0bb5cc5
Michael W Powell <mwpowellhtx@gmail.com>
2019-05-24 17:45:21 -0400

    added option extension methods

---
+++ src/NConsole.Options/Extensions/OptionExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿namespace NConsole.Options
+{
+    using static OptionValueType;
+
+    /// <summary>
+    /// Provides a set of helpful <see cref="IOption"/> extension methods.
+    /// </summary>
+    public static class OptionExtensionMethods
+    {
+        /// <summary>
+        /// Returns whether <paramref name="option"/> Is <see cref="Required"/>.
+        /// </summary>
+        /// <param name="option"></param>
+        /// <returns></returns>
+        public static bool IsRequired(this IOption option) => option?.ValueType == Required;
+    }
+}
