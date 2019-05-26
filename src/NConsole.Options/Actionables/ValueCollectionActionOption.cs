5dfb37c723ddd0c2edd4aff79bad19397264d417
Michael W Powell <mwpowellhtx@gmail.com>
2019-05-25 21:07:49 -0400

    poised to remove ValueCollectionActionOption altogether we do not think it adds value so to speak and there are better ways to capture a range of values via options

--- src/NConsole.Options/Actionables/ValueCollectionActionOption.cs
+++ src/NConsole.Options/Actionables/ValueCollectionActionOption.cs
@@ -1,18 +1,18 @@
-﻿namespace NConsole.Options
-{
-    // TODO: TBD: still not sure what this one has to do with?
-    /// <summary>
-    /// Represents an <see cref="OptionValueCollection"/> based <see cref="Option"/> asset.
-    /// </summary>
-    /// <inheritdoc />
-    internal sealed class ValueCollectionActionOption : ActionOptionBase<OptionValueCollection>
-    {
-        /// <inheritdoc />
-        public ValueCollectionActionOption(string prototype, string description, int count, OptionCallback<OptionValueCollection> callback)
-            : base(prototype, description, count, callback)
-        {
-        }
+﻿//namespace NConsole.Options
+//{
+//    // TODO: TBD: still not sure what this one has to do with?
+//    /// <summary>
+//    /// Represents an <see cref="OptionValueCollection"/> based <see cref="Option"/> asset.
+//    /// </summary>
+//    /// <inheritdoc />
+//    internal sealed class ValueCollectionActionOption : ActionOptionBase<OptionValueCollection>
+//    {
+//        /// <inheritdoc />
+//        public ValueCollectionActionOption(string prototype, string description, int count, OptionCallback<OptionValueCollection> callback)
+//            : base(prototype, description, count, callback)
+//        {
+//        }
 
-        // TODO: TBD: do we need to handle this one different during OnInvocation?
-    }
-}
+//        // TODO: TBD: do we need to handle this one different during OnInvocation?
+//    }
+//}
